851415245b8ffdfdbbf819c58c285d3a85c20895
LinkK9 <linhgtx@gmail.com>
2021-03-17 18:05:43 +0700

    add some methods

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@
     }
 
     // de-button
+
+   
+
     $(".minus-button").on("click", function () {
         let quantityInput = +$(this).next().val();
         if (quantityInput > 1) {
@@ -22,9 +25,21 @@
 
         // update total sum
         let totalSum = $(this).parent().parent().parent().parent().parent().next().find(".sumary-estimate td:last-child");
-        let totalProduct = 
-        totalSum.text()
-        
+        let productsSumArray = $(".sumary-of-quantity").toArray();
+        let inputsArray = $(".quantity-input").toArray();
+        let numberOfProducts = $(this).parent().parent().parent().parent().parent().next().find(".total-products td:last-child");
+
+        let sumInput = 0
+        for(let i = 0; i < inputsArray.length; i++) {
+            sumInput += +inputsArray[i].value.replace(".000đ", "");
+        };
+        numberOfProducts.text(sumInput + " Sản phẩm");
+
+        let sumItem = 0;
+        for(let i = 0; i < productsSumArray.length; i++) {
+            sumItem += +productsSumArray[i].innerText.replace(".000đ", "");
+        };
+        totalSum.text(sumItem +  ".000đ");
     });
 
     // in-button
@@ -37,6 +52,24 @@
         let price = $(this).parent().parent().next().find(".product-price").text().replace(".000đ", "");
         let sumary = $(this).parent().parent().next().next().find(".sumary-of-quantity");
         sumary.text((quantityInput + 1) * price + ".000đ");
+
+        // update total sum
+        let totalSum = $(this).parent().parent().parent().parent().parent().next().find(".sumary-estimate td:last-child");
+        let productsSumArray = $(".sumary-of-quantity").toArray();
+        let inputsArray = $(".quantity-input").toArray();
+        let numberOfProducts = $(this).parent().parent().parent().parent().parent().next().find(".total-products td:last-child");
+        
+        let sumInput = 0
+        for(let i = 0; i < inputsArray.length; i++) {
+            sumInput += +inputsArray[i].value.replace(".000đ", "");
+        };
+        numberOfProducts.text(sumInput + " Sản phẩm");
+
+        let sumItem = 0;
+        for(let i = 0; i < productsSumArray.length; i++) {
+            sumItem += +productsSumArray[i].innerText.replace(".000đ", "");
+        };
+        totalSum.text(sumItem +  ".000đ");
     });
 
 
@@ -49,4 +82,37 @@
 
 
 
-// Cal total prices
+// Delete row item
+    $(".delete-item").on("click", function(){
+        let productNamne = $(this).parent().parent().find("p.product-name").text();
+        let row = $(this).parent().parent();
+        if(confirm(`Bạn muốn xoá sản phẩm ${productNamne} ?`)) {
+            row.remove();
+        }
+
+        // update total sum
+        let totalSum = $(".sumary-estimate td:last-child");
+        let productsSumArray = $(".sumary-of-quantity").toArray();
+        let inputsArray = $(".quantity-input").toArray();
+        let numberOfProducts = $(".total-products td:last-child");
+
+        let sumInput = 0
+        for(let i = 0; i < inputsArray.length; i++) {
+            sumInput += +inputsArray[i].value.replace(".000đ", "");
+        };
+        numberOfProducts.text(sumInput + " Sản phẩm");
+
+        let sumItem = 0;
+        for(let i = 0; i < productsSumArray.length; i++) {
+            sumItem += +productsSumArray[i].innerText.replace(".000đ", "");
+        };
+        totalSum.text(sumItem +  ".000đ");
+
+        if(sumItem == 0){
+            $(".payment").css("display", "none");
+            $(".add-cart").css("display", "block");
+            $(".add-cart").attr('style', 'width: 100% !important');
+        }
+    });
+
+
